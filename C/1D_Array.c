2bc0c2e89b6a24cd4cca6d9ac6a497d89ace6a15
joshuabg96 <josuebg96@hotmail.com>
2025-02-17 11:44:18 -0600

    1D Arrays in C Exercise is complete

---
+++ C/1D_Array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n, sum = 0;
+    scanf("%d", &n);
+    fflush(stdin);
+    int *arr = (int*)malloc(n*sizeof(int));
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+    free(arr);
+    printf("%d", sum);    
+    return 0;
+}
